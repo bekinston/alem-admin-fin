c68ee1b370e72d617d06f6f4d7d33d6ebe0c3476
bekinston <bekt884@gmail.com>
2022-03-24 04:47:32 +0600

    try

--- routes/mail.routes.js
+++ routes/mail.routes.js
@@ -4,7 +4,6 @@ const router = Router()
 router.post('/send', async (req, res) => {
     try {
         const {email} = req.body
-
         res.status(201).json({ message:'email: ' + email })
     } catch (e) {
         res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
