fa29e12e7c4113bb4ade4ce9a0d3e29366f8ef36
bekinston <bekt884@gmail.com>
2022-04-11 23:48:56 +0600

    sort fix 4

--- routes/go.routes.js
+++ routes/go.routes.js
@@ -85,7 +85,7 @@ router.post('/addfilm', async (req, res) => {
             day2 = '0' + day2;
     var startdate1 = [day1, month1, year1].join('.')
     var enddate1 = [day2, month2, year2].join('.')
-    const go = await Film.updateOne({name}, {$set: {state : "go", promo : promo, cost: cost, startdate: startdate1, enddate:enddate1 }})
+    const go = await Film.updateOne({name}, {$set: {state : "go", promo : promo, cost: cost, startdate: startdate1, enddate:enddate1, start:Date.now() }})
     res.status(201).json({ message:'Фильм добавлен' })
   } catch (e) {
     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
@@ -94,7 +94,7 @@ router.post('/addfilm', async (req, res) => {
 
 router.get('/', async (req, res) => {
   try {
-    const films = await Film.find({ state: "go" }).sort({startdate:-1})
+    const films = await Film.find({ state: "go" }).sort({start:-1})
     res.json(films)
   } catch (e) {
     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
