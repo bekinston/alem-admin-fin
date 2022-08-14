6bd59ca1776b236f2292d1dd5123b479d5412ed1
bekinston <bekt884@gmail.com>
2022-08-14 10:02:09 +0600

    Initial commit

--- client/src/components/SearchFilm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const SearchFilm = () => {
-  return(
-    <div>
-      <h1>FilmPage</h1>
-    </div>
-  )
-}
--- client/src/pages/FilmUpload.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const FilmUpload = () => {
-  return (
-    <div>
-      <h1>FilmUpload</h1>
-    </div>
-  )
-}
